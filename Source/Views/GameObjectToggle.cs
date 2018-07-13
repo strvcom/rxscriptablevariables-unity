4bfb6e4af1e29dbf59c09b94a5baeece31889cc1
Martin Novak <martin.novak@strv.com>
2018-07-13 11:03:57 +0200

    Initial commit

---
+++ Source/Views/GameObjectToggle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+#if REACTIVE_VARIABLE_RX_ENABLED
+using UniRx;
+#endif
+
+namespace WolarGames.Variables.Views
+{
+    public class GameObjectToggle : MonoBehaviour
+    {
+        public BoolReference Variable;
+
+        /// Should this be hidden when toggle is on
+        public bool HideOnToggle;
+
+        private void Awake() {
+            
+#if REACTIVE_VARIABLE_RX_ENABLED
+            Variable.AsObservable().Subscribe((bool value) =>
+            {
+                gameObject.SetActive(!HideOnToggle && value || HideOnToggle && !value);
+            }).AddTo(this);
+#endif
+        }
+    }
+}
---
+++ Source/Views/VariableToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if REACTIVE_VARIABLE_RX_ENABLED
+using UniRx;
+#endif
+
+namespace WolarGames.Variables.Views
+{
+    [RequireComponent(typeof(Toggle))]
+    public class VariableToggle : MonoBehaviour
+    {
+        public BoolVariable Variable;
+
+        void Start() {
+            var toggle = GetComponent<Toggle>();
+            
+#if REACTIVE_VARIABLE_RX_ENABLED
+            toggle.OnValueChangedAsObservable().Subscribe(value =>
+            {
+                Variable.CurrentValue = value;
+            }).AddTo(this);
+
+            Variable.AsObservable().Subscribe(value =>
+            {
+                toggle.isOn = value;
+            }).AddTo(this);
+#endif
+        }
+    }
+}
